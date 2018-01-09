cfe49591991c9b60d925c9a56e06483e2eb27158
506_2 <506_2@mail.ru>
2018-01-09 15:04:20 +0300

    Add Componets to form Result

--- TestProjectApp/TestProjectApp/Result.cs
+++ TestProjectApp/TestProjectApp/Result.cs
@@ -16,5 +16,10 @@ namespace TestProjectApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
