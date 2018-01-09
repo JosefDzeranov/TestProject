3840ed8313711e76d587a7b79a7772ed26a22a2b
506_2 <506_2@mail.ru>
2018-01-09 15:26:28 +0300

    Add calc right Answer Count

--- TestProjectApp/TestProjectApp/Result.Designer.cs
+++ TestProjectApp/TestProjectApp/Result.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -47,18 +48,31 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(16, 38);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(264, 212);
+            this.listBox1.Size = new System.Drawing.Size(264, 160);
             this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Result
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Name = "Result";
             this.Text = "Result";
+            this.Load += new System.EventHandler(this.Result_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,5 +82,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- TestProjectApp/TestProjectApp/Result.cs
+++ TestProjectApp/TestProjectApp/Result.cs
@@ -21,5 +21,29 @@ namespace TestProjectApp
         {
 
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Result_Load(object sender, EventArgs e)
+        {
+            int rightAnswerCount = 0;
+            for (int i = 0; i < Program.numberOfQuestions; i++)
+            {
+                listBox1.Items.Add(i.ToString() + " " + Program.rightanswer[i] + " : " + Program.hisanswer[i]);
+                if (Program.rightanswer[i] == Program.hisanswer[i]) 
+                {
+                    rightAnswerCount++; 
+                }
+            }
+            label2.Text ="Кол-во правильных ответов: " + rightAnswerCount.ToString(); 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
