ce5ee83afe58d4006f6bcb85a6e982e33adff7af
ThaneCrios <thanecrios041@gmail.com>
2018-01-09 14:04:00 +0300

    change form design

---
+++ TestProjectApp/TestProjectApp/Author.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestProjectApp
+{
+    public partial class Author : Form
+    {
+        public Author()
+        {
+            InitializeComponent();
+        }
+
+        private void Author_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Иосиф" && textBox2.Text == "Дзеранов")
+            {
+                MessageBox.Show("Успешная авторизация!");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверные данные!");
+                this.Close();
+            }
+        }
+    }
+}
